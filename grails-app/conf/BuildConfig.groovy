a99e47a32e7d048d7704bec6a63355cbd610bf55
ikakara <allen.arakaki@gmail.com>
2015-03-26 13:30:20 -0700

    update to grails 2.5.0

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -34,7 +34,7 @@ grails.project.dependency.resolution = {
       export = false
     }
 
-    build(":release:3.1.0", ":rest-client-builder:2.0.3") {
+    build(":release:3.1.1", ":rest-client-builder:2.1.1") {
       export = false
     }
   }
