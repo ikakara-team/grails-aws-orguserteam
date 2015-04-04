a610f2328a5db7ebf949bea9dcc9d3d3de2d7343
ikakara <allen.arakaki@gmail.com>
2015-04-04 09:50:52 -0700

    bump version

--- AwsOrguserteamGrailsPlugin.groovy
+++ AwsOrguserteamGrailsPlugin.groovy
@@ -5,7 +5,7 @@ import ikakara.orguserteam.dao.dynamo.IdEmail
 import ikakara.simplemarshaller.web.app.SimpleMarshallerService
 
 class AwsOrguserteamGrailsPlugin {
-  def version = "0.4"
+  def version = "0.4.1"
   def grailsVersion = "2.2 > *"
   def loadAfter = ['aws-instance']
   def pluginExcludes = [
