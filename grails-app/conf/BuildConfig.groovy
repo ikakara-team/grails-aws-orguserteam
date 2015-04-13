e631653c92c293bcf5aae3b1b1d8c3f4320d7c4f
ikakara <allen.arakaki@gmail.com>
2015-04-13 12:50:53 -0700

    bump version

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -30,7 +30,7 @@ grails.project.dependency.resolution = {
     compile ':plugin-config:0.2.0'
 
     compile ':simple-marshaller:0.1.2'
-    compile (':aws-instance:0.5') {
+    compile (':aws-instance:0.5.1') {
       export = false
     }
 
